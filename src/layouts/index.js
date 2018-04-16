aa9e4c2f36b41fb79dabf7f0c812f78596bf140f
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-04-16 09:51:59 +0200

    Add webfont

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { canUseDOM } from 'exenv';
 import 'normalize.css';
 
 import Navbar from '../components/Navbar';
 
 import './index.css';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title='Home | Gatsby + Netlify CMS' />
-    <Navbar />
-    <div>{children()}</div>
-  </div>
-)
+class TemplateWrapper extends Component {
+  componentWillMount() {
+    if (canUseDOM) {
+      const WebFont = require('webfontloader');
+      WebFont.load({
+        google: {
+          families: ['Karla:400,700']
+        }
+      });
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Helmet title='Förändringen' />
+        <Navbar />
+        <div>{children()}</div>
+      </div>
+    );
+  }
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
